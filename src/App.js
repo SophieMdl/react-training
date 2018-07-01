d600b14e73dcda311f20ca1b832516c2d1835a34
SophieMdl <sophie.michaudel@gmail.com>
2018-07-01 11:42:14 +0200

    add reach-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import Music from './containers/Music.js'
+import Prime from './containers/Prime.js'
+import Graph from './containers/Graph.js'
+import { Router, Link } from "@reach/router";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Music/>
+        <nav>
+          <Link to="music">Music</Link>
+          <Link to="prime">Prime</Link>
+          <Link to="graph">Graph</Link>
+        </nav>
+        <Router>
+          <Music path="/music"/>
+          <Prime path="/prime"/>
+          <Graph path="/graph"/>
+        </Router>
       </div>
     );
   }
